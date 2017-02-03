d3f4279e82786b7d23f0d1eda706cd7171263af0
jamalparit <jamalparit@outlook.com>
2017-02-03 16:54:15 +0800

    add Demo File DB dan Upload

--- import_excel.php
+++ import_excel.php
@@ -1,26 +1,13 @@
 <?php
-//$hostname = “localhost”;
-//$username = “root”;
-//$password = “”;
-//$database = “import_exceldata”;
-//$conn = new mysqli(“$hostname”,”$username”,”$password”);
-//mysql_select_db(“$database”, $conn) or die(mysql_error());
-
-$servername = "localhost";
+$hostname = "localhost";
 $username = "root";
 $password = "";
-$dbname = "import_exceldata";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-	
+$database = "import_exceldata";
+$conn = new mysqli("$hostname","$username","$password","$database") or die(mysql_error());
 ?>
 <html>
 <head>
-<style type=”text/css”>
+<style type="text/css">
 body
 {
 margin: 0;
@@ -58,38 +45,35 @@ background-color: #FCF3F3;
 
 </head>
 <body>
-<div class=”top-bar”>
-<div class=”inside-top-bar”>Import Excelsheet Data in mysql table<br><br>
+<div class="top-bar">
+<div class="inside-top-bar">Import Excelsheet Data in mysql table<br><br>
 </div>
 </div>
-<div style=”text-align:left; border:1px solid #333333; width:300px; margin:0 auto; padding:10px;”>
+<div style="text-align:left; border:1px solid #333333; width:300px; margin:0 auto; padding:10px;">
 
-<form name=”import” method=”post” enctype=”multipart/form-data”>
-<input type=”file” name=”file” /><br />
-<input type=”submit” name=”submit” value=”Submit” />
+<form name="import" method="post" enctype="multipart/form-data">
+<input type="file" name="file" /><br />
+<input type="submit" name="submit" value="Submit" />
 </form>
 <?php
-if(isset($_POST[“submit”]))
+if(isset($_POST["submit"]))
 {
 $file = $_FILES[‘file’][‘tmp_name’];
-$handle = fopen($file, “r”);
+$handle = fopen($file, "r");
 $c = 0;
-while(($filesop = fgetcsv($handle, 1000, “,”)) !== false)
+while(($filesop = fgetcsv($handle, 1000, ",")) !== false)
 {
 $name = $filesop[0];
 $project = $filesop[1];
 
-//$sql = mysql_query(“INSERT INTO mytask (name, project) VALUES (‘$name’,’$project’)”);
-$suruh = “INSERT INTO mytask (name, project) VALUES (‘$name’,’$project’)”;
-$sql = mysql_query($suruh)
-
+$sql = mysql_query("INSERT INTO mytask (name, project) VALUES (‘$name’,’$project’)");
 $c = $c + 1;
 }
 
 if($sql){
-echo “You database has imported successfully. You have inserted “. $c .” recoreds”;
+echo "You database has imported successfully. You have inserted ". $c ." recoreds";
 }else{
-echo “Sorry! There is some problem.”;
+echo "Sorry! There is some problem.";
 }
 
 }
